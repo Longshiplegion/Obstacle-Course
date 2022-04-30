0f1d6b94b6a355a3416178d184c85a73a09927d8
longshiplegion <amyers0107@gmail.com>
2022-04-29 21:14:02 -0600

    Update to game
    Added cinemachine camera, and made it follow player, added walls and a rigiid body to have basic collision

--- Assets/Mover.cs
+++ Assets/Mover.cs
@@ -1,9 +1,13 @@
+using System.Threading;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
 public class Mover : MonoBehaviour
 {
+     [SerializeField] float moveSpeed = 0f;
+    
+    
     // Start is called before the first frame update
     void Start()
     {
@@ -12,7 +16,11 @@ public class Mover : MonoBehaviour
 
     // Update is called once per frame
     void Update()
-    {
+    {   
         
+        float xValue = Input.GetAxis("Horizontal") * Time.deltaTime * moveSpeed;//move left and right
+        float zValue = Input.GetAxis("Vertical") * Time.deltaTime * moveSpeed;//move up and down
+        
+        transform.Translate(xValue,0,zValue);
     }
 }
