d6f2335ab5e277bc15f6d6fc226ab8106b53de99
longshiplegion <amyers0107@gmail.com>
2022-04-29 23:05:41 -0600

    Added collision to game
    Added instructions and moved playermovement into a functions. Added a new script that prints to console when the player hits a wall.

--- Assets/Mover.cs
+++ Assets/Mover.cs
@@ -11,13 +11,23 @@ public class Mover : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-        
+        PrintInstructions();
     }
 
     // Update is called once per frame
     void Update()
     {   
-        
+        MovePlayer();
+    }
+    void PrintInstructions()
+    {
+        Debug.Log("Welcome to the game");
+        Debug.Log("Move your player by using the WASD keys or the arrow keys");
+        Debug.Log("The Objective of the game is to avoid the obstacles and reach the end");
+    }
+    
+    void MovePlayer()
+    {
         float xValue = Input.GetAxis("Horizontal") * Time.deltaTime * moveSpeed;//move left and right
         float zValue = Input.GetAxis("Vertical") * Time.deltaTime * moveSpeed;//move up and down
         
